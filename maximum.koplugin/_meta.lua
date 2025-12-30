5bb9240550098f37822d5a9d9ee541cfee88161a
Shac0x <dherrera.98@outlook.com>
2025-12-30 22:52:00 +0100

    Version added

--- maximum.koplugin/_meta.lua
+++ maximum.koplugin/_meta.lua
@@ -4,4 +4,5 @@ return {
     fullname = _("Maximum Grid View"),
     description = _([[Divides the screen into 4 cells for manga reading. Pinch zoom on any cell to expand it to fullscreen. Only works with CBZ and CBR files.]]),
     credits = _("@Shac0x"),
+    version = "1.2.0",
 }
